9b96edc89ab6102a383d2a704029e5b97617bb1d
刀哥 <liufan321@gmail.com>
2016-03-15 13:17:37 +0800

    modify text demo

--- FFLabel/FFLabelObjcDemo/FFLabelObjcDemo/ViewController.m
+++ FFLabel/FFLabelObjcDemo/FFLabelObjcDemo/ViewController.m
@@ -18,9 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.label.labelDelegate = self;
-    self.label.text = @"#FFLabel#This is a @FFLabel Demo, access http://github.com/liufan321/fflabel can get the demo project. Follow @liufan2000 to get more information.";
+    FFLabel *s = [[FFLabel alloc] initWithFrame:CGRectZero];
+    
+    [self.view addSubview:s];
+    
+    s.labelDelegate = self;
+    s.text = @"#FFLabel#This is a @FFLabel Demo, access http://github.com/liufan321/fflabel can get the demo project. Follow @liufan2000 to get more information.";
+    
+    [s setPreferredMaxLayoutWidth:[UIScreen mainScreen].bounds.size.width - 20];
+    s.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[s]-10-|" options:0 metrics:nil views:@{@"s": s}]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:s attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:100]];
+    s.numberOfLines = 0;
 }
+
 
 - (void)labelDidSelectedLinkText:(FFLabel * __nonnull)label text:(NSString * __nonnull)text {
     NSLog(@"%@", text);
